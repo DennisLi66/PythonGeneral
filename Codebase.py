57e8c110b99da9d3d5df441ac4532ee2fb87aae1
Dennis Li <li.dennis.beta@gmail.com>
2020-08-31 14:21:56 -0700

    main menu update

--- Codebase.py
+++ Codebase.py
@@ -5,15 +5,16 @@ while (True):
     print("Welcome to Dennis' misc Python codes");
     print('Press [0] for the number guesser');
     print('Press [1] for the simple phonebook');
+    print('[Q]uit/[q]uit');
     choice = input();
     if (choice == '0'):
         print("You selected Number Guesser!");
         numberGuesser.numberGuesser();
-    if (choice == '1'):
+    elif (choice == '1'):
         print("You selected Simple Phonebook");
-        simpPBK.populate();
         simpPBK.runBook();
-
-
+    elif ((choice == 'Q') or (choice == 'q')):
+        break;
+    
 
 
--- simplePhonebook.py
+++ simplePhonebook.py
@@ -85,6 +85,3 @@ def runBook():
 
 
 
-
-
-runBook();
