7ceba093a8093e265928f2e4c3eb2ceebb2cf7bf
Dennis Li <li.dennis.beta@gmail.com>
2020-09-08 03:47:47 -0700

    found title and url

--- feelingLucky.py
+++ feelingLucky.py
@@ -1,25 +1,59 @@
-import requests
+import requests, webbrowser, urllib.parse
 import bs4
 import os
 
-def openPage():
-    return;
+def prettyPrint(superList):
+    for x in range(len(superList)):
+        print("[" + str(x) + "]:" + superList[x][0]);
+        print("[" + str(x) + "]:" + superList[x][1]);
+
+def cleanMessyURL(superList):
+    for i in range(len(superList)):
+        c = superList[i][1][7:];
+        #print(superList[i][1][7:])
+        rex = c.rfind("&sa=U&ved=")
+        c = c[0:rex]
+        c = urllib.parse.unquote(c)
+        superList[i] = (superList[i][0],c);
+        
+
 
 def obtainHTMLfromURL(url):
     print("URL: " + url);
     res = requests.get(url)
     if (res.status_code != requests.codes.ok):
         raise Exception("Error Code: " + res.status_code);
-    bSObj = bs4.BeautifulSoup(res.text);
-    links = bSObj.select('r');
-    print(links);
+    bS = bs4.BeautifulSoup(res.text,"html.parser");
+    #bS = bS.prettify();
+    #links = bS.find_all('div div div', attrs={'class':'r'});
+    superList = [];
 
-def createListofLinks(text):
+    listsC = bS.findAll("div", {"class": "ZINbbc xpd O9g5cc uUPGi"})
+    listsD = [];
+    listsA = [];
+    listsB = []
+    for i in range(len(listsC)):
+        plac = listsC[i].findAll("div", {"class": "kCrYT"})
+       # print(len(plac) > 0);
+        if (len(plac) > 0):
+            listsD.append(plac[0])
+    for u in range(len(listsD)):
+        if (len(listsD[u].findAll("div", {"class": "BNeawe vvjwJb AP7Wnd"})) > 0):
+            listsA.append(listsD[u].findAll("div", {"class": "BNeawe vvjwJb AP7Wnd"})[0])
+        if (len(listsD[u].select('a')) > 0):
+            listsB.append(listsD[u].select('a')[0])
     
 
+    for l in range(len(listsA)):
+        superList.append((listsA[l].getText(),listsB[l].get('href')))
+        #print(listsA[l])
+        #print(listsB[l])
+    return superList;
+    
+    
 def produceGoogleLink(words):
     link = "https://www.google.com/search?q=";
-    combo = '_'.join(words.split());
+    combo = '+'.join(words.split());
     return link + combo;
 
 def interface():
@@ -31,7 +65,12 @@ def interface():
         if ((choice == "Q") or (choice == "q")):
             break;
         elif ((choice == "P") or (choice == "p")):
-
+            print("What would you like to search?")
+            words = input();
+            url = produceGoogleLink(words);
+            mList = obtainHTMLfromURL(url);
+            cleanMessyURL(mList);
+            prettyPrint(mList);
         else:
             print("Sorry, didn't catch that. You're going to have to try again.")
 
