b7b7518265c49227d7c2cf78794558e1e80d9d3c
Dennis Li <li.dennis.beta@gmail.com>
2020-09-10 02:09:39 -0700

    finishing the google site spawner

--- Codebase.py
+++ Codebase.py
@@ -1,10 +1,13 @@
 import numberGuesser
-import simplePhonebook as simpPBK 
+import simplePhonebook as simpPBK
+import feelingLucky as GoogleSpawn
 
+
+print("Welcome to Dennis' misc Python codes");
 while (True):
-    print("Welcome to Dennis' misc Python codes");
     print('Press [0] for the number guesser');
     print('Press [1] for the simple phonebook');
+    print('Press [2] for the Google Search Site Spawner');
     print('[Q]uit/[q]uit');
     choice = input();
     if (choice == '0'):
@@ -13,8 +16,14 @@ while (True):
     elif (choice == '1'):
         print("You selected Simple Phonebook");
         simpPBK.runBook();
+    elif (choice == '2'):
+        print("You selected Google Search Site Spawner");
+        GoogleSpawn.interface();
     elif ((choice == 'Q') or (choice == 'q')):
         break;
+    else:
+        print("Sorry, that was not a valid option.")
+    
     
 
 
--- feelingLucky.py
+++ feelingLucky.py
@@ -4,8 +4,8 @@ import os
 
 def prettyPrint(superList):
     for x in range(len(superList)):
-        print("[" + str(x) + "]:" + superList[x][0]);
-        print("[" + str(x) + "]:" + superList[x][1]);
+        print("[" + str(x) + "]:" + superList[x][0] + " (" + superList[x][1] + ")");
+        #print("[" + str(x) + "]:" + superList[x][1]);
 
 def cleanMessyURL(superList):
     for i in range(len(superList)):
@@ -50,16 +50,17 @@ def obtainHTMLfromURL(url):
         #print(listsB[l])
     return superList;
     
-    
-def produceGoogleLink(words):
+
+def produceGoogleLink(words,offset = 0):
     link = "https://www.google.com/search?q=";
     combo = '+'.join(words.split());
-    return link + combo;
+    return link + combo + "&start=" + str(offset);
 
 def interface():
-    print("Welcome to the I'm Feeling Lucky Spawner.")
+    print("Welcome to the Google Search Site Spawner.")
+    offset = 0;
     while (True):
-        print("[P]roduce/[p]roduce ten pages from Google's I'm feeling lucky.")
+        print("[P]roduce/[p]roduce the first page of results from Google Search.")
         print("[Q]uit/[q]uit")
         choice = input();
         if ((choice == "Q") or (choice == "q")):
@@ -68,11 +69,58 @@ def interface():
             print("What would you like to search?")
             words = input();
             url = produceGoogleLink(words);
-            mList = obtainHTMLfromURL(url);
-            cleanMessyURL(mList);
-            prettyPrint(mList);
+            try:
+                mList = obtainHTMLfromURL(url);
+            except:
+                print("Something went wrong. Returning to hub.")
+            if (len(mList) == 0):
+                print("Unfortunately, no results for " + words + " exists.")
+            else:
+                cleanMessyURL(mList);
+                while (True):
+                    print("\nType a given number to open the associated website.")
+                    print("[O]pen/[o]pen all sites shown.");
+                    prettyPrint(mList);
+                    if (offset > 0):
+                        print("[P]revious/[p]revious page")
+                    print("[N]ext/[n]ext page")
+                    print("[C]ancel/[c]ancel");
+                    iChoice = input();
+                    if ((iChoice == "C") or (iChoice == "c")):
+                        print('\n')
+                        break;
+                    elif (iChoice.isnumeric()):
+                        num = int(iChoice);
+                        if ((num < 0) or (num >= len(mList))):
+                            print("Sorry, that isn't an applicable number.")
+                        else:
+                            print("Opening: " + mList[num][1]);
+                            webbrowser.open(mList[num][1])
+                    elif ((iChoice == "O") or (iChoice == "o")):
+                        for tupl in mList:
+                            print("Opening: " + tupl[1]);
+                            webbrowser.open(tupl[1]);
+                    elif ((iChoice == "N") or (iChoice == "n")):
+                        offset += 10;
+                        url = produceGoogleLink(words,offset);
+                        try:
+                            nList = obtainHTMLfromURL(url);
+                        except:
+                            print("Something went wrong. Returning to hub.")
+                        if (len(nList) == 0):
+                            print("Unfortunately, no more results for " + words + " exist.")
+                            offset -= 10;
+                        else:
+                            mList = nList;
+                            cleanMessyURL(mList);
+                    elif (((iChoice == "P") or (iChoice == "p")) and offset > 0):
+                        offset -= 10;
+                        url = produceGoogleLink(words,offset);
+                        try:
+                            mList = obtainHTMLfromURL(url);
+                            cleanMessyURL(mList);
+                        except:
+                            print("Something went wrong. Returning to hub.")
         else:
             print("Sorry, didn't catch that. You're going to have to try again.")
-
-
-interface();
+            
